e26b21aa8dee1754d42ad0817189305ace20eb89
Larry Peng <peng.larry@outlook.com>
2025-06-10 11:39:04 -0400

    Fix: add working Java practice files and ignore .class files

--- MyOwnPractice/JavaPrac1.java
+++
@@ -1,8 +0,0 @@
-Public class JavaPrac1 {
-    public static void main(String[] args) {
-        int number = "10";
-        for(int i = 0; i <= 5; i++) {
-            System.out.println("The number is: " + number)
-        }
-    }
-}
---
+++ MyOwnPractice/JavaPrac1/JavaPrac1.java
@@ -0,0 +1,8 @@
+public class JavaPrac1 {
+    public static void main(String[] args) {
+        int number = 10;
+        for(int i = 0; i <= 5; i++) {
+            System.out.println("The number is: " + number);
+        }
+    }
+}
